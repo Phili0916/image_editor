f317ac472b62a34eb962eaec81db727d02daf021
Phili0916 <79313259+Phili0916@users.noreply.github.com>
2023-11-08 22:37:48 +0100

    print image format

--- photo_editor.py
+++ photo_editor.py
@@ -1,10 +1,11 @@
 from PIL import Image, ImageEnhance, ImageFilter
 import os
 
-path = '/unedited_images'
-path_out = '/edited_images'
+path = 'unedited_images'
+path_out = 'edited_images'
 
 for photo in os.listdir(path):
-    image = Image.open(f"(path)/(photo)")
+    image = Image.open(f"{path}/{photo}")
+    print(image.format, image.size, image.mode)
 
-    edit = image.filter(ImageFilter.SHARPEN)
+    edit = image.filter(ImageFilter.SHARPEN).rotate(-90)
