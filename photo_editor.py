d324356be9b5a95aa4af4a6ceaa34e1753e411b6
Phili0916 <79313259+Phili0916@users.noreply.github.com>
2023-11-09 18:59:44 +0100

    Corrected Save to specific Path

--- photo_editor.py
+++ photo_editor.py
@@ -1,8 +1,8 @@
-from PIL import Image, ImageEnhance, ImageFilter
+from PIL import Image, ImageEnhance, ImageFilter, ImageChops
 import os
 
-path = 'unedited_images'
-path_out = 'edited_images'
+path = './unedited_images'
+edited_path = './edited_images'
 
 for photo in os.listdir(path):
     image = Image.open(f"{path}/{photo}")
@@ -11,10 +11,15 @@ for photo in os.listdir(path):
     edit = image.filter(ImageFilter.SHARPEN).rotate(-90)
     print(f"edit{edit}")
 
+    ## Create a New Image
+    new_image = Image.new('RGBA', (1000, 600))
+    new_image.show()
+
     ## Color enhancer
     color_enhancer = ImageEnhance.Color(image)
     print(f"color_enhancer: {color_enhancer}")
-    color_enhancer.enhance(5.0).show()
+    color_enhancer.enhance(5.0)
+    color_enhancer.image.save(f"{edited_path}/color_enhanced_photo.jpg")
 
 
     ## Contrast enhancer
